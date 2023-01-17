b62b9b797176f30d0570da52a4451e32c9814072
Miteshvaghela <vaghela_miteshk@yahoo.com>
2023-01-17 12:58:10 +0530

    update

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 const About = ({props}) => {
     return (
         <div>
-            <h3 style={{margin:'40px 0px', textAlign:'center'}}>This is footer section of the app.</h3>
+            <h4 style={{margin:'40px 0px', textAlign:'center'}}>This is footer section of the app.</h4>
         </div>
     )
 }
